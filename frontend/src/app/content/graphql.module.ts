1cd34b69cea09089478a31825a1eb015de1ea1f5
BenjaminGuzman <bg@benjaminguzman.dev>
2022-12-12 17:30:17 -0600

    [frontend]: Added notice to the bottom right of index

--- frontend/src/app/content/graphql.module.ts
+++ frontend/src/app/content/graphql.module.ts
@@ -1,14 +1,29 @@
 import {NgModule} from '@angular/core';
-import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloModule, APOLLO_OPTIONS, APOLLO_NAMED_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 import {environment} from "../../environments/environment";
 
-const uri = environment.contentUrl;
+export function createNamedApollo(httpLink: HttpLink): Record<string, ApolloClientOptions<any>> {
+  return {
+    content: {
+      name: "content",
+      link: httpLink.create({uri: environment.contentUrl}),
+      cache: new InMemoryCache()
+    },
+    cart: {
+      name: "cart",
+      link: httpLink.create({uri: environment.cartUrl}),
+      cache: new InMemoryCache()
+    }
+  }
+}
+
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
-    link: httpLink.create({uri}),
+    link: httpLink.create({uri: environment.contentUrl}),
     cache: new InMemoryCache(),
+    name: "content"
   };
 }
 
@@ -16,10 +31,16 @@ export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   exports: [ApolloModule],
   providers: [
     {
+      // default apollo client
       provide: APOLLO_OPTIONS,
       useFactory: createApollo,
       deps: [HttpLink],
     },
+    {
+      provide: APOLLO_NAMED_OPTIONS,
+      useFactory: createNamedApollo,
+      deps: [HttpLink]
+    }
   ],
 })
 export class GraphQLModule {
--- frontend/src/app/content/tracks/track/track.component.ts
+++ frontend/src/app/content/tracks/track/track.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Track} from "./track";
+import {Apollo, gql} from "apollo-angular";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-track',
@@ -10,7 +12,31 @@ export class TrackComponent implements OnInit {
   @Input()
   public trackData!: Track;
 
-  constructor() { }
+  constructor(private apollo: Apollo) { }
+
+  add2Cart(trackId: number) {
+    const subscription: Subscription = this.apollo.use("cart").mutate<{addTracks: number}>({
+      mutation: gql`mutation addTracks($ids: [ID!]!) {
+        addTracks(ids: $ids)
+      }`,
+      variables: {
+        ids: [trackId]
+      }
+    }).subscribe({
+      next: (res) => {
+        subscription.unsubscribe();
+        // TODO HANDLE RESPONSE
+        console.log(res.data);
+      },
+      error: (err: Error) => {
+        subscription.unsubscribe();
+        // TODO HANDLE RESPONSE
+        console.error("Failed to add tracks");
+        console.error(err);
+      },
+      complete: () => subscription.unsubscribe()
+    })
+  }
 
   ngOnInit(): void {
   }
