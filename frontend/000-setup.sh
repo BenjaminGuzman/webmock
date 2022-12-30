536494f5d78a98b2c1b0d4f79720405f65942111
BenjaminGuzman <bg@benjaminguzman.dev>
2022-12-30 15:06:23 -0600

    Fixed errors with gateway container image

--- frontend/000-setup.sh
+++ frontend/000-setup.sh
@@ -1,17 +1,27 @@
-#!/usr/bin/env sh
+#!/bin/sh
+
 if [ -z "$GATEWAY_BASE_URL" ]; then
-  echo "ERROR: Environment variable GATEWAY_GATEWAY_BASE_URL is not set!"
-  exit 1
+	echo "ERROR: Environment variable GATEWAY_GATEWAY_BASE_URL is not set!"
+	exit 1
 fi
 
 if [ "$INCLUDE_V1" = "true" ] || [ "$INCLUDE_V1" = "t" ]; then # Note: this line is actually correct, is POSIX syntax (https://github.com/koalaman/shellcheck/wiki/SC2039)
-  CONFIG_FILE="/etc/nginx/conf.d/gateway.conf"
-  CONFIG_START=$(more "$CONFIG_FILE" | grep --line-number "### BEGIN V1 CONFIG ###" | cut --delimiter ':' --fields 1)
-  CONFIG_END=$(more "$CONFIG_FILE" | grep --line-number "### END V1 CONFIG ###" | cut --delimiter ':' --fields 1)
+	echo Including v1 request forwarding in configuration...
 
-  UNCOMMENT_START=$((CONFIG_START + 1))
-  UNCOMMENT_END=$((CONFIG_END - 1))
-  sed -i "$UNCOMMENT_START,$UNCOMMENT_END s/^\(\s*\)#/\1/g" "$CONFIG_FILE" # remove all comments
+	CONFIG_FILE="/etc/nginx/conf.d/gateway.conf"
+	CONFIG_START=$(more "$CONFIG_FILE" | grep -n "### BEGIN V1 CONFIG ###" | cut -d ':' -f 1)
+	CONFIG_END=$(more "$CONFIG_FILE" | grep -n "### END V1 CONFIG ###" | cut -d ':' -f 1)
+
+	UNCOMMENT_START=$((CONFIG_START + 1))
+	UNCOMMENT_END=$((CONFIG_END - 1))
+	sed -i "$UNCOMMENT_START,${UNCOMMENT_END}s/^\(\s*\)#/\1/g" "$CONFIG_FILE" # remove all comments
+
+	echo Done.
+else
+	echo Not including v1 request forwarding
+	echo If you want to forward requests to v1
+	echo set environment variable
+	echo INCLUDE_V1=true
 fi
 
 find /var/www/html/webmock -not -type d -exec sed -i "s|https://test.benjaminguzman.dev|$GATEWAY_BASE_URL|g" {} +
